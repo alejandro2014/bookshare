98f3b5abbe6cca81e1c063df4e0e09c87662ee95
Alejandro <alejandroruperez@gmail.com>
2020-01-29 16:30:30 +0100

    Added the controller endpoint

--- src/main/java/org/bookshare/api/controllers/BookController.java
+++ src/main/java/org/bookshare/api/controllers/BookController.java
@@ -1,16 +1,18 @@
 package org.bookshare.api.controllers;
 
-import org.bookshare.api.entities.Book;
 import org.bookshare.api.entities.Library;
+import org.bookshare.api.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class BookController {
+    @Autowired
+    private BookService bookService;
+
     @GetMapping("/books")
-    public List<Book> getBooksFromLibrary(Library library) {
-        return null;
+    public Library getBooksFromPerson(Integer personId) {
+        return bookService.getBooksFromPersonId(personId);
     }
 }
--- src/main/java/org/bookshare/api/entities/Book.java
+++ src/main/java/org/bookshare/api/entities/Book.java
@@ -1,8 +1,10 @@
 package org.bookshare.api.entities;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Book {
     private String title;
     private String author;
